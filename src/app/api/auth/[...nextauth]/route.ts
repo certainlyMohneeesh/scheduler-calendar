1097de76cff9b98bcacc8394cfd459a04825b03f
CertainlyMohneesh <133652369+certainlyMohneeesh@users.noreply.github.com>
2024-11-10 02:42:31 +0530

    auth try 2

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,12 @@
 import NextAuth from "next-auth"
 import { authOptions } from "@/lib/auth"
 
-const handler = NextAuth(authOptions)
+const handler = NextAuth({
+  ...authOptions,
+  callbacks: {
+    async redirect() {
+      return '/calendar'
+    }
+  }
+})
 export { handler as GET, handler as POST }
